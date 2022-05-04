fa9db6ec9b6d7c83b1118eb81ff925308d202abd
Zeroo7 <92607753+Zeroo7@users.noreply.github.com>
2022-05-04 18:00:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ const body = document.querySelector('body');
 const container = document.querySelector('.container');
 
 // APIs
-const weatherURL = `http://api.openweathermap.org/data/2.5/weather?units=metric&`;
+const weatherURL = `https://api.openweathermap.org/data/2.5/weather?units=metric&`;
 const urlGeo = `https://api.ipregistry.co/?${keys.geoKey}`;
-const positionURL = `http://api.positionstack.com/v1/reverse?${keys.positionKey}&limit=1&query=`;
+const positionURL = `https://api.bigdatacloud.net/data/reverse-geocode-client?`;
 
 
 // get compass direction
@@ -30,7 +30,6 @@ locationButton.addEventListener('click', () => {
 
 const showError = (error) => {
     target.classList.add('htmlFalse')
-    console.log(error.message);
     switch (error.code) {
         case error.PERMISSION_DENIED:
             target.innerHTML = "<h3>Please allow location access</h3>"
@@ -50,13 +49,11 @@ const showError = (error) => {
 const showPosition = (position) => {
     target.innerHTML = `<img id="loading" src="Imgs/loading.png">`
     setTimeout(() => {
-        console.log(position)
         const url = positionURL + position.coords.latitude + ',' + position.coords.longitude
         fetch(url).then((data) => data.json())
             .then((data) => {
-                console.log(data);
-                const city = data.data[0].county;
-                const country = data.data[0].country;
+                const city = data.locality;
+                const country = data.countryCode;
                 passData(position, city, country)
             })
             .catch((error) => reqError(error))
@@ -70,7 +67,6 @@ const GetIPData = () => {
         fetch(urlGeo)
             .then((data) => data.json())
             .then((data) => {
-                console.log(data);
                 const city = data.location.city;
                 const country = data.location.country.name;
                 passData(data, city, country)
@@ -88,12 +84,10 @@ search.addEventListener('click', () => {
         const url = weatherURL + 'q=' + city + keys.weatherKey
         fetch(url).then((wethD) => wethD.json())
             .then((wethD) => {
-                console.log(wethD);
                 const country = wethD.sys.country;
                 update(wethD, city, country)
             })
             .catch((error) => {
-                console.log(error.message);
                 if (error.message === 'Failed to fetch') {
                     target.innerHTML = `<h3>Please check your connection and try again</h3>`
                     target.classList.add('htmlFalse')
@@ -113,7 +107,6 @@ const passData = (data, city, country) => {
         const url = weatherURL + 'q=' + city + keys.weatherKey
         fetch(url).then((wethD) => wethD.json())
             .then((wethD) => {
-                console.log(wethD);
                 update(wethD, city, country)
             })
             .catch((error) => reqError(error))
@@ -122,7 +115,6 @@ const passData = (data, city, country) => {
         const url = weatherURL + 'lat=' + data.coords.latitude + '&lon=' + data.coords.longitude + keys.weatherKey
         fetch(url).then((wethD) => wethD.json())
             .then((wethD) => {
-                console.log(wethD);
                 update(wethD, city, country)
             })
             .catch((error) => reqError(error))
@@ -149,7 +141,6 @@ const update = (wethD, city, country) => {
 const reqError = (error) => {
     target.innerHTML = `<h3>Someting went wrong, please check your connection or try again later</h3>`
     target.classList.add('htmlFalse')
-    console.log(error);
 }
 
 document.addEventListener('DOMContentLoaded', GetIPData)
